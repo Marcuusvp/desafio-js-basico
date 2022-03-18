566841390bca5af2cb7c0a8a05533b95d1d0b03e
Marcus Vinicius Pereira <marcusviniciuspereira1@gmail.com>
2022-03-18 11:44:06 -0300

    Quest JS básico DevQuest

---
+++ index.js
@@ -0,0 +1,29 @@
+var numeroUm, numeroDois
+
+function soma(numeroUm, numeroDois) {
+    let soma = numeroUm + numeroDois
+    console.log('O valor da soma é: ' + soma)
+}
+
+soma(2, 2)
+
+function subtracao(numeroUm, numeroDois) {
+    let subtracao = numeroUm - numeroDois
+    console.log('O valor da subtração é: ' + subtracao)
+}
+
+subtracao(9, 3)
+
+function multiplicacao(numeroUm, numeroDois) {
+    let multiplicacao = numeroUm * numeroDois
+    console.log('O valor da multiplicação é: ' + multiplicacao)
+}
+
+multiplicacao(5, 5)
+
+function divisao(numeroUm, numeroDois) {
+    let divisao = numeroUm / numeroDois
+    console.log('O valor da divisão é: ' + divisao)
+}
+
+divisao(25, 5)
